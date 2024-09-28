f7c016955910b1322e3a3ac68847d5f30e5f25d2
AndresB101996 <andres.f19@outlook.com>
2024-09-27 21:25:52 -0700

    Version 2

--- app.py
+++ app.py
@@ -3,12 +3,11 @@ import pandas as pd
 import plotly_express as px
 
 car_data = pd.read_csv("C:/Users/Andre/Documents/Andres/Self-learning/Bootcamp/Sprint6/Project/vehicles_us.csv") #read data
+
+st.header('Sprint 6 Project: Software development tools') # Creación del titulo
 build_histogram = st.checkbox('Construir un histograma')
 build_scatter = st.checkbox('Construir un diagrama de dispersión')
 
-
-st.header('Sprint 6 Project: Software development tools')
-        
 if build_histogram: # mark the check box
     # Write a message
     st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
