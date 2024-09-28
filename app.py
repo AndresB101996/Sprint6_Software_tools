92c5f6c2dade9b48b8e11b87b6e960c093ab17fb
AndresB101996 <andres.f19@outlook.com>
2024-09-27 21:55:21 -0700

    Version 3

--- app.py
+++ app.py
@@ -4,10 +4,32 @@ import plotly_express as px
 
 car_data = pd.read_csv("C:/Users/Andre/Documents/Andres/Self-learning/Bootcamp/Sprint6/Project/vehicles_us.csv") #read data
 
-st.header('Sprint 6 Project: Software development tools') # Creación del titulo
+
+st.header('Sprint 6 Project: Software development tools') # Se crea el titulo de la pagina
+
+st.dataframe(data=car_data) #Se muestra el dataframe
+
+st.header('Histograma condicion vs modelo ') 
+
+
+
+# Crear un filtro con selectbox
+model_selected = st.selectbox('Condicion', car_data['condition'].unique())
+
+# Filtrar el DataFrame basado en la categoría seleccionada
+df_filtered = car_data[car_data['condition'] == model_selected]
+
+#plot histogram
+fig = px.histogram(df_filtered, x="model_year")
+       
+# interactive Plotly graph
+st.plotly_chart(fig, use_container_width=True,)
+
 build_histogram = st.checkbox('Construir un histograma')
 build_scatter = st.checkbox('Construir un diagrama de dispersión')
 
+
+        
 if build_histogram: # mark the check box
     # Write a message
     st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
